f3d3ee14e1bb2310278d62b5dca62978d180d380
Bryan Melvida <126201239+BLMgithub@users.noreply.github.com>
2026-01-15 21:26:23 +0800

    Add 01_data_cleaning, module and docs Docs: dataset and preprocessing log Module: assessment_views

---
+++ docs/preprocessing_log.md
@@ -0,0 +1,8 @@
+# Preprocessing Log
+## 1. Data Quality & Field Handling
+
+**Doc Scope:** Field-level documentation of data quality issues, corrections, and data handling decisions applied during preprocessing.
+
+**Related script:** [`01_data_cleaning.ipynb`](../notebooks/01_data_cleaning.ipynb)
+
+**Output dataset:** [`customer_data.csv`]()
---
+++ src/assessment_views.py
@@ -0,0 +1,81 @@
+# =============================================================================
+# Visual assessment helpers
+# =============================================================================
+
+
+import pandas as pd
+import numpy as np
+from tabulate import tabulate
+
+
+
+def df_summary(dataframe: pd.DataFrame):
+    """
+    Return a consolidated, read-only summary for visual data assessment.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+
+    Returns
+    -------
+    pandas.DataFrame
+        Column-level summary including:
+        - dtype
+        - non-null count
+        - null count
+        - null percentage
+        - unique values.
+    """
+
+    rows, cols = dataframe.shape
+
+    null_counts = dataframe.isna().sum()
+    non_null_counts = rows - null_counts
+
+    info = pd.DataFrame({
+        'data type': dataframe.dtypes,
+        '# unique': dataframe.nunique(),
+        '# non-null': non_null_counts,
+        '# null': null_counts,
+        '% null': (null_counts / rows) * 100
+    })
+
+    print(f'Total Rows: {rows:,}')
+    print(f'Total Columns: {cols}')
+    print(f'Total Null Values: {null_counts.sum():,}')
+    print()
+
+    return print(info)
+
+
+
+
+def print_tabulate(data: pd.Series | pd.DataFrame, heading: str = (), title: str = None):
+
+    """
+    Prints a formatted table using the tabulate library, with an optional heading and title.
+
+    Preset parameters
+    -----------------
+    - table format: 'pretty'
+    - show index: 'False'
+
+    headers:
+    --------
+        can be an explicit list of column headers
+        - if `headers="firstrow"`, then the first row of data is used
+        - if `headers="keys"`, then dictionary keys or column indices are used
+
+    All other parameters follow the default behavior of the `tabulate` library.
+    """
+
+    print(title)
+    print(
+        tabulate(
+            tabular_data = data,
+            headers = heading,
+            tablefmt = 'pretty',
+            showindex = 'False'
+        )
+    )
